e312f2a4b6690fc739dac570e4ae428bbcb39af1
Perse <Perse@PC>
2020-09-10 20:02:11 -0300

    Win e Lin

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -10,83 +10,182 @@ public class RedesController {
 		super();
 	}
 	
-	public void ip (String so) {
-		try {
-			int i = 0;
-			int ips = 0;
-			int Nomes = 0;
-			String linha;
-/*			String Nome[] = new String[i];	linha: 37 */
-/*			String ip[] = new String[ips];	linha: 38 */
-			Process p = Runtime.getRuntime().exec("ipconfig");
-			InputStream fluxo = p.getInputStream();
-			InputStreamReader leitor = new InputStreamReader(fluxo);
-			BufferedReader buffer = new BufferedReader(leitor);
-			while (i < 4) {
-				linha = buffer.readLine();
-				i++;
-			}
-			linha = buffer.readLine();
-			while (linha != null) {
-				if (linha.contains("Adaptador Ethernet")) {
-					Nomes++;
-				} else if (linha.contains("IPv4")) {
-					ips++;
-				}
-				linha = buffer.readLine();
-				if (linha == null) {
+	public void ip(String so) {
+		
+/////////// Windows //////////////////////////////////////////////////////////////
+		
+		if (so.contains("Win")) {
+			try {
+				int i = 0;
+				int ips = 0;
+				int Nomes = 0;
+				String linha;
+				/* String Nome[] = new String[i]; linha: 37 */
+				/* String ip[] = new String[ips]; linha: 38 */
+				Process p = Runtime.getRuntime().exec("ipconfig");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				while (i < 4) {
 					linha = buffer.readLine();
+					i++;
 				}
-			}
-			String Nome[] = new String[Nomes];
-			String ip[] = new String[ips];
-			buffer.close();
-			leitor.close();
-			fluxo.close();
-			
+				linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("Adaptador Ethernet")) {
+						Nomes++;
+					} else if (linha.contains("IPv4")) {
+						ips++;
+					}
+					linha = buffer.readLine();
+					if (linha == null) {
+						linha = buffer.readLine();
+					}
+				}
+				String Nome[] = new String[Nomes];
+				String ip[] = new String[ips];
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+
 ///////////	Segunda chamada do processo ip ///////////////////////////////////////
-			
-			i = 0;
-			ips = 0;
-			Nomes = 0;
-			Process p2 = Runtime.getRuntime().exec("ipconfig");
-			InputStream fluxo2 = p2.getInputStream();
-			InputStreamReader leitor2 = new InputStreamReader(fluxo2);
-			BufferedReader buffer2 = new BufferedReader(leitor2);
-			while (i < 4) {
-				linha = buffer2.readLine();
-				i++;
-			}
-			linha = buffer2.readLine();
-			while (linha != null) {
-				if (linha.contains("Adaptador Ethernet")) {
-					Nome[Nomes] = linha;
-					Nomes++;
-				} else if (linha.contains("IPv4")) {
-					ip[ips] = linha.substring(49);
-					ips++;
-				}
-				linha = buffer2.readLine();
-				if (linha == null) {
+
+				i = 0;
+				ips = 0;
+				Nomes = 0;
+				Process p2 = Runtime.getRuntime().exec("ipconfig");
+				InputStream fluxo2 = p2.getInputStream();
+				InputStreamReader leitor2 = new InputStreamReader(fluxo2);
+				BufferedReader buffer2 = new BufferedReader(leitor2);
+				while (i < 4) {
 					linha = buffer2.readLine();
+					i++;
 				}
-			}
-			buffer2.close();
-			leitor2.close();
-			fluxo2.close();
-			
-			for (i = 0; i < ips; i++) {
-				System.out.println(Nome[i]);
-				System.out.println(ip[i]);
-			}
+				linha = buffer2.readLine();
+				while (linha != null) {
+					if (linha.contains("Adaptador Ethernet")) {
+						Nome[Nomes] = linha;
+						Nomes++;
+					} else if (linha.contains("IPv4")) {
+						ip[ips] = linha.substring(49);
+						ips++;
+					}
+					linha = buffer2.readLine();
+					if (linha == null) {
+						linha = buffer2.readLine();
+					}
+				}
+				buffer2.close();
+				leitor2.close();
+				fluxo2.close();
+
+				for (i = 0; i < ips; i++) {
+					System.out.println(Nome[i]);
+					System.out.println(ip[i]);
+				}
 ///////////	Fim da segunda chamada do processo ip ////////////////////////////////
+
+			} catch (Exception e) {
+				String msgError = e.getMessage();
+				System.err.println(msgError);
+			}
 			
-		} catch (Exception e) {
-			String msgError = e.getMessage();
-			System.err.println(msgError);
+///////////	Linux ////////////////////////////////////////////////////////////////		
+			
+		} else if (so.contains("Lin")) {
+			try {
+				int i = 0;
+				int y = 0;
+				int sim = 0;
+				int ips = 0;
+				int Nomes = 0;
+				String linha;
+				/* String Nome[] = new String[i]; linha: 37 */
+				/* String ip[] = new String[ips]; linha: 38 */
+				Process p = Runtime.getRuntime().exec("ifconfig");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("mtu")) {
+						Nomes++;
+					} else if (linha.contains("netmask")) {
+						ips++;
+					}
+					linha = buffer.readLine();
+					if (linha == null) {
+						linha = buffer.readLine();
+					}
+				}
+				String Nome[] = new String[Nomes];
+				String ip[] = new String[ips];
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+
+///////////	Segunda chamada do processo ip ///////////////////////////////////////
+
+				i = 0;
+				ips = 0;
+				Nomes = 0;
+				Process p2 = Runtime.getRuntime().exec("ifconfig");
+				InputStream fluxo2 = p2.getInputStream();
+				InputStreamReader leitor2 = new InputStreamReader(fluxo2);
+				BufferedReader buffer2 = new BufferedReader(leitor2);
+				linha = buffer2.readLine();
+				while (linha != null) {
+					if (linha.contains("mtu")) {
+						Nome[Nomes] = linha;
+						Nomes++;
+					} else if (linha.contains("netmask")) {
+						ip[ips] = linha;
+						ips++;
+					}
+					linha = buffer2.readLine();
+					if (linha == null) {
+						linha = buffer2.readLine();
+					}
+				}
+				buffer2.close();
+				leitor2.close();
+				fluxo2.close();
+				
+				for (i=0; i<ips; i++) {
+					y = 0;
+					String[] vetorNome = Nome[i].split(" ");
+					for (String IpNome : vetorNome) {
+						if(y == 0) {
+							System.out.println(IpNome);
+							y++;
+						}
+						i++;
+					}
+				}
+				
+				System.out.println(" ");
+				System.out.println("ipv4 respectivos: ");
+				System.out.println(" ");
+				
+				for (i=0; i<ips; i++) {
+					String[] vetorIp = ip[i].split(" ");
+					for (String Ip : vetorIp) {
+						if (sim == 1) {
+							System.out.println(Ip);
+						} else if (Ip.contains("inet")) {
+							sim = 1;
+						}
+						i++;
+					}
+				}
+///////////	Fim da segunda chamada do processo ip ////////////////////////////////
+
+			} catch (Exception e) {
+				String msgError = e.getMessage();
+				System.err.println(msgError);
+			}
 		}
 	}
-	
 	
 	public void ping(String so) {
 
